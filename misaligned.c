59d4971125775cb30c99c1f73e361d55500c3128
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-23 16:26:26 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,8 +2,6 @@
 #include <assert.h>
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -13,7 +11,9 @@ int printColorMap() {
     return i * j;
 }
 void printOnConsole(int majorColorIndex, int minorColorIndex) {
-    printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColor[majorColorIndex], minorColor[majorColorIndex]);
 }
 int main() {
     int result = printColorMap();
