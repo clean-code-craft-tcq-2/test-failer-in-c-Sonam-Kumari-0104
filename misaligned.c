e76da3d05003f9acfadb19db23144c924bd60d82
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-25 21:26:45 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <assert.h>
 
-void printOnConsole(int majorColorIndex, int minorColorIndex) {
-     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+typedef Struct {
+     int pairNumber;
+     char majorColor;
+     char minorColor;
+}colorPair;
+
+colorPair testPrintcolorMapOnConsole() {
+     for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+             pairNumber = i * 5 + j;
+             colorPair.majorColor[i * 5 + j] = majorColor[i];
+             colorPair.majorColor[i * 5 + j] = minorColor[j];
+        }
+    }
+    return colorPair;
+}
+
+void printColorMapOnConsole(int majorColorIndex, int minorColorIndex) {
     printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColor[majorColorIndex], minorColor[majorColorIndex]);
 }
 
@@ -19,7 +37,13 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
-    assert(result == 24);
+    assert(result == 25);
+    colorPair c1[25];
+    c1 = testPrintcolorMapOnConsole();
+    assert(c1.pairNumber == 25);
+    assert(strcmp(c1.majorColor[4], majorColor[4]));
+    assert(strcmp(c1.minorColor[4], minorColor[4]));
+    assert
     printf("All is well (maybe!)\n");
     return 0;
 }
