e62ebb69ef68f60dd6be0255a89fd5a7bcbbf69b
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-25 22:08:31 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -9,18 +9,13 @@ typedef struct {
      int pairNumber;
      const char* majorColor[8];
      const char* minorColor[8];
-}colorPair[25];
+}colorPair;
 
-colorPair testPrintcolorMapOnConsole() {
+colorPair testPrintcolorMapOnConsole(int pno, const char* majorCol, const char* minorCol) {
      colorPair colorMap;
-     int i = 0, j = 0;
-     for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-             colorMap.pairNumber = i * 5 + j;
-             colorMap.majorColor[i * 5 + j] = majorColor[i];
-             colorMap.minorColor[i * 5 + j] = minorColor[i];
-        }
-    }
+     colorMap.pairNumber = pno;
+     colorMap.majorColor[pno] = majorCol;
+     colorMap.minorColor[pno] = minorCol;
     return colorMap;
 }
 
@@ -41,12 +36,11 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    colorPair c1[25];
-    c1 = testPrintcolorMapOnConsole();
+    colorPair c1;
+    c1 = testPrintcolorMapOnConsole(16, "Yellow", "Orange");
     assert(c1.pairNumber[16] == 16);
-    assert(strcmp(c1.majorColor[16], "Yellow"));
-    assert(strcmp(c1.minorColor[16], "Orange"));
-    
+    assert(strcmp(c1.majorColor[16], "Yellow")==0);
+    assert(strcmp(c1.minorColor[16], "Orange")==0);    
     printf("All is well (maybe!)\n");
     return 0;
 }
