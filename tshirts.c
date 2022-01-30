48e3ea2550f8a5505dbe623acf64e4600a696f18
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-01-30 20:39:25 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -13,12 +13,21 @@ char size(int cms) {
     return sizeName;
 }
 
+void testTshirtsSize(int sizeInCms, char sizeInChar) {
+    assert(size(sizeInCms)==sizeInChar);
+}
+
 int main() {
-    assert(size(37) == 'S');
-    assert(size(38) == 'S');
-    assert(size(40) == 'M');
-    assert(size(42) == 'M');
-    assert(size(43) == 'L');
+    //assert(size(37) == 'S');
+    testTshirtsSize(37, 'S');
+    //assert(size(38) == 'S');
+    testTshirtsSize(38, 'S');
+    //assert(size(40) == 'M');
+    testTshirtsSize(40, 'M');
+    //assert(size(42) == 'M');
+    testTshirtsSize(42, 'M');
+    //assert(size(43) == 'L');
+    testTshirtsSize(43, 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
