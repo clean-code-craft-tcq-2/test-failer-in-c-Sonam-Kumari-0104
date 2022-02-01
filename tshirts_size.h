e067a880cffc0f6ac1e46c30c8fa0a12519b4e7a
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-01 19:10:35 +0530

    Create tshirts_size.h

---
+++ tshirts_size.h
@@ -0,0 +1,11 @@
+/* Macros for all the size limits */
+#define S_MINLIMIT 34
+#define S_MAXLIMIT 38
+#define M_MINLIMIT 38
+#define M_MAXLIMIT 42
+#define L_MINLIMIT 42
+#define L_MAXLIMIT 44
+
+/* Function Prototypes */
+char size(int cms);
+void testTshirtsSize(int sizeInCms, char sizeInChar);
