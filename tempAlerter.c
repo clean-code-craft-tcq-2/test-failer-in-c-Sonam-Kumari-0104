41c59aa8a25870e173feb6bb125aa878b33cd9a6
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-01 20:17:22 +0530

    Update and rename alerter.c to tempAlerter.c

--- tempAlerter.c
+++ tempAlerter.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define TEMP_THRESHOLD 200
-int alertFailureCount = 0;
-static int failureCounter = 0;
-
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    if ( celcius <= TEMP_THRESHOLD )
-    {
-        return 200;
-    }
-    else 
-    {
-        failureCounter++;
-        return 500;
-    }
-}
-
 float farenheitToCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     return celcius;
 }
 
-void alertInCelcius(float farenheit) {
+void alertInCelcius(float farenheit, int (*fnPtrForNetworkAlert(float))) {
     float celcius = farenheitToCelcius(farenheit);
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = (*fnPtrForNetworkAlert)(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -40,6 +20,7 @@ void alertInCelcius(float farenheit) {
 
 int main() {
     alertInCelcius(400.5);
+    test_tempAlertImpl(400.5);
     alertInCelcius(303.6);
     alertInCelcius(600);
     assert(alertFailureCount==failureCounter);
