3fd0f51cdaecfada1681c8157edaf69a09011d57
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-01 20:21:52 +0530

    Update tempAlerter.h

--- tempAlerter.h
+++ tempAlerter.h
@@ -1,8 +1,6 @@
 #define TEMP_THRESHOLD 200
-int alertFailureCount = 0;
-static int failureCounter = 0;
 
 int networkAlertStub(float celcius);
 float farenheitToCelcius(float farenheit);
-void alertInCelcius(float farenheit, int (*fnPtrForNetworkAlert(float)));
+void alertInCelcius(float farenheit, int (*fnPtrForNetworkAlert)(float));
 void test_tempAlertImpl(float fahrenheit);
