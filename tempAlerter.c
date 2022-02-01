77fad07b117afe2487149d7729204551a420a611
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-01 20:25:16 +0530

    Update tempAlerter.c

--- tempAlerter.c
+++ tempAlerter.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <assert.h>
 
 int alertFailureCount = 0;
-static int failureCounter = 0;
 
 float farenheitToCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
@@ -19,14 +17,4 @@ void alertInCelcius(float farenheit, int (*fnPtrForNetworkAlert)(float)) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount +=1;
     }
-}
-
-int main() {
-    test_tempAlertImpl(400.5);
-    test_tempAlertImpl(303.6);
-    test_tempAlertImpl(600);
-    assert(alertFailureCount==failureCounter);
-    printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well (maybe!)\n");
-    return 0;
 }
