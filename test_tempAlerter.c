c124b8f767e55de28240cabdccaad27b85171a22
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-01 20:25:19 +0530

    Update test_tempAlerter.c

--- test_tempAlerter.c
+++ test_tempAlerter.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <assert.h>
 #include "tempAlerter.h"
+
+static int failureCounter = 0;
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
@@ -20,4 +23,13 @@ int networkAlertStub(float celcius) {
 void test_tempAlertImpl(float fahrenheit) {
   alertInCelcius(fahrenheit, networkAlertStub);
   assert(alertFailureCount==failureCounter);
+}
+
+int main() {
+    test_tempAlertImpl(400.5);
+    test_tempAlertImpl(303.6);
+    test_tempAlertImpl(600);
+    printf("%d alerts failed.\n", alertFailureCount);
+    printf("All is well (maybe!)\n");
+    return 0;
 }
